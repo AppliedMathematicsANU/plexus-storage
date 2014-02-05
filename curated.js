2c8bcc0774192bb31059083f85d82e6bc59a0060
odf <olaf.delgado@gmail.com>
2014-02-05 17:48:58 +1100

    generate and log timestamps for curated

--- curated.js
+++ curated.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var timestamp = require('monotonic-timestamp');
-
 var cc   = require('ceci-core');
 var chan = require('ceci-channels');
 var cf   = require('ceci-filters');
@@ -14,23 +12,23 @@ var decode = util.decode;
 const END = '\xff';
 
 
-var putAttr = function(batch, entity, attr, val, attrSchema) {
-  batch.put(encode(['eav', entity, attr, val]), '');
-  batch.put(encode(['aev', attr, entity, val]), '');
+var putAttr = function(batch, entity, attr, val, attrSchema, timestamp) {
+  batch.put(encode(['eav', entity, attr, val]), timestamp);
+  batch.put(encode(['aev', attr, entity, val]), timestamp);
   if (attrSchema.indexed)
-    batch.put(encode(['ave', attr, val, entity]), '');
+    batch.put(encode(['ave', attr, val, entity]), timestamp);
   if (attrSchema.reference)
-    batch.put(encode(['vae', val, attr, entity]), '');
+    batch.put(encode(['vae', val, attr, entity]), timestamp);
 };
 
 
-var delAttr = function(batch, entity, attr, val, attrSchema) {
+var delAttr = function(batch, entity, attr, val, attrSchema, timestamp) {
   batch.del(encode(['eav', entity, attr, val]))
   batch.del(encode(['aev', attr, entity, val]));
   if (attrSchema.indexed)
     batch.del(encode(['ave', attr, val, entity]));
   if (attrSchema.reference)
-    batch.del(encode(['vae', val, attr, entity]), '');
+    batch.del(encode(['vae', val, attr, entity]));
 };
 
 
@@ -59,6 +57,15 @@ module.exports = function(storage, schema) {
         }));
     };
 
+    var makeTimestamp = function() {
+      return cc.go(function*() {
+        var t = yield chan.pull(scan('seq'));
+        var next = (t === undefined) ? -1 : t.key[0] - 1;
+        yield storage.write(encode(['seq', next]), Date.now());
+        return next;
+      });
+    };
+
     var readAttributes = function(entity) {
       return cc.go(function*() {
         var result = {};
@@ -73,16 +80,17 @@ module.exports = function(storage, schema) {
 
     var writeAttributes = function(entity, attr) {
       return cc.go(function*() {
+        var t = yield makeTimestamp();
         var batch = storage.batch();
         var key;
 
         var old = yield readAttributes(entity);
         for (key in old)
           if (attr.hasOwnProperty(key))
-            delAttr(batch, entity, key, old[key], attrSchema(key));
+            delAttr(batch, entity, key, old[key], attrSchema(key), t);
 
         for (key in attr)
-          putAttr(batch, entity, key, attr[key], attrSchema(key));
+          putAttr(batch, entity, key, attr[key], attrSchema(key), t);
 
         return yield batch.write();
       });
@@ -90,17 +98,18 @@ module.exports = function(storage, schema) {
 
     var destroy = function(entity) {
       return cc.go(function*() {
+        var t = yield makeTimestamp();
         var batch = storage.batch();
 
         var old = yield readAttributes(entity);
         for (var key in old)
-          delAttr(batch, entity, key, old[key], attrSchema(key));
+          delAttr(batch, entity, key, old[key], attrSchema(key), t);
 
         yield chan.each(
           function(item) {
             var attr = item.key[0];
             var other = item.key[1];
-            delAttr(batch, other, attr, entity, attrSchema(attr));
+            delAttr(batch, other, attr, entity, attrSchema(attr), t);
           },
           scan('vae', entity));
 
